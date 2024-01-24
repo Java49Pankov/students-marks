e78703b45d3d6438371e20f00d0316ba09fbd8e6
Evgeny Pankov <volvovnl123@gmail.com>
2024-01-24 17:08:00 +0700

    initial code

---
+++ src/main/java/telran/students/service/StudentsService.java
@@ -0,0 +1,17 @@
+package telran.students.service;
+
+import java.util.*;
+
+import telran.students.dto.*;
+
+public interface StudentsService {
+	Student addStudent(Student student);
+
+	Student updatePhone(long id, String phone);
+
+	List<Mark> addMark(long id, Mark mark);
+
+	Student removeStudent(long id);
+
+	List<Mark> getMarks(long id);
+}
